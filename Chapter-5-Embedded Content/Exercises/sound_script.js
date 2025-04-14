3eb7e4f87bb1d79e2d8b96d9c0490fd6b0cd8d34
Giennelle Akiat <rainbowshinegaming@gmail.com>
2025-04-14 19:40:36 +0400

    Chapter 5 - Soundboard

--- Chapter-5-Embedded Content/Exercises/sound_script.js
+++ Chapter-5-Embedded Content/Exercises/sound_script.js
@@ -1,15 +1,15 @@
-function textToAudio() {
-    let msg = document.getElementById("text-to-speech").value;
-    let speech = new SpeechSynthesisUtterance();
-    speech.lang = "en-US";
-    speech.text = msg;
-    speech.volume = 1;
-    speech.rate = 1;
-    speech.pitch = 1;
-    window.speechSynthesis.speak(speech);
-}
-
+    function textToAudio() {
+        let msg = document.getElementById("text-to-speech").value;
+        let speech = new SpeechSynthesisUtterance();
+        speech.lang = "en-US";
+        speech.text = msg;
+        speech.volume = 1;
+        speech.rate = 1;
+        speech.pitch = 1;
+        window.speechSynthesis.speak(speech);
+    }
+    
 function play_sound(clicked_id) {
-    var audio = new Audio("Audio Sampler/Audio" + clicked_id + ".mp3");
+    var audio = new Audio("Audio Sampler/Audio/" + clicked_id + ".mp3");
     audio.play();
   }
